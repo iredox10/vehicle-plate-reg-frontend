06b9a4d53f3775504f64eac720b923ec0c0bed2a
iredox <idreesadam200@gmail.com>
2024-09-06 16:45:42 +0100

    remove / in path

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,4 +1,4 @@
 // const path = 'http://localhost:3002'
-const path = 'https://vehicle-plate-reg-backend.onrender.com/'
+const path = 'https://vehicle-plate-reg-backend.onrender.com'
 
 export default path
