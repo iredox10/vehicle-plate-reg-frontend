c70025342c59a3ecbc09978b11197fd7e12f1cd2
iredox <idreesadam200@gmail.com>
2024-09-07 12:06:30 +0100

    fix issuer login error

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -5,18 +5,21 @@ import { useState } from "react";
 import FormBtn from "../components/FormBtn";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import { ErrorMsg } from "../components/ErrorMsg";
 
 const AdminLogin = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [err, setErr] = useState("");
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const res = await axios.post(`${path}/admin/login`,{email,password});
-      const admin = res.data
-      navigate(`/admin/${admin._id}`)
-    } catch (err) {
+      const res = await axios.post(`${path}/admin/login`, { email, password });
+      const admin = res.data;
+      navigate(`/admin/${admin._id}`);
+    } catch (error) {
+      setErr(error.response.data.error);
       console.log(err);
     }
   };
@@ -31,7 +34,7 @@ const AdminLogin = () => {
           <h1 className="bg-green-700 p-2 text-white font-bold text-2xl">
             Login
           </h1>
-
+          {err && <ErrorMsg err={err} />}
           <FormInput
             type={"text"}
             labelFor={"email"}
--- src/pages/IssuerLogin.jsx
+++ src/pages/IssuerLogin.jsx
@@ -5,20 +5,29 @@ import { useState } from "react";
 import FormBtn from "../components/FormBtn";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import { ErrorMsg } from "../components/ErrorMsg";
 
 const IssuerLogin = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [err, setErr] = useState("");
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    if (!email || !password) {
+      setErr("fields can't be empty");
+      return;
+    }
     try {
-      const res = await axios.post(`${path}/issuer/login`,{email,password});
-      const issuer = res.data
-      navigate(`/issuer/${issuer._id}`)
-      console.log(issuer)
-    } catch (err) {
-      console.log(err);
+      const res = await axios.post(`${path}/issuer/login`, { email, password });
+      const issuer = res.data;
+      if (res.status === 200) {
+        console.log(res.data);
+        navigate(`/issuer/${issuer._id}`);
+      }
+    } catch (error) {
+      setErr(error.response.data.message);
+      // console.log(error.response.data.message);
     }
   };
   return (
@@ -29,8 +38,9 @@ const IssuerLogin = () => {
       </h1>
       <div className="bg-green-400 mx-3 my-6 p-2 md:w-2/4 md:mx-auto">
         <h1 className="bg-green-700 p-2 text-white font-bold text-2xl">
-           Login
+          Login
         </h1>
+        {err && <ErrorMsg err={err} />}
         <form onSubmit={handleSubmit}>
           <FormInput
             type={"text"}
